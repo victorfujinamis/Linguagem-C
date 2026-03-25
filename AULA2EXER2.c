eaa86bb791cc9c45595116cf57fb3f0e8e67a0b1
victorfujinamis <victorfujinamis@gmail.com>
2026-03-25 10:00:00 -0300

    Add files via upload

---
+++ AULA2EXER2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int ano_atual = 2026;
+    int ano_nascimento = 2007;
+    int idade = ano_atual - ano_nascimento;
+    printf("A idade e: %d anos\n", idade);
+    return 0;
+}
